c734050d02c441478f20f4a26f9251a2f5062ada
vladimir <lovkiy2012@gmail.com>
2020-10-02 21:13:44 +0300

    53 creating a slider on the site. Slider slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,47 +1,19 @@
 console.log('test');
 $(document).ready(function () {
   $('.carousel__inner').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
     prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
-    variableWidth: true,
-    // responsive: [
-    //   {
-    //     breakpoint: 992,
-    //     settings: {
-    //       arrows: false,
-    //       dots: true,
-    //     },
-    //   },
-    // ],
-    slidesToShow: 2,
-    slidesToScroll: 1,
+
     responsive: [
       {
-        breakpoint: 1000,
+        breakpoint: 992,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
+          arrows: false,
           dots: true,
         },
       },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
     ],
   });
 });
