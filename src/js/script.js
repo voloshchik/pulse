e8351ebc5a04a031f42bcf05a3b447545171590f
vladimir <lovkiy2012@gmail.com>
2020-10-06 20:36:41 +0300

    55 tiny-slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,35 @@
 console.log('test');
-$(document).ready(function () {
-  $('.carousel__inner').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
+// $(document).ready(function () {
+//   $('.carousel__inner').slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
+//     nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
 
-    responsive: [
-      {
-        breakpoint: 992,
-        settings: {
-          arrows: false,
-          dots: true,
-        },
-      },
-    ],
-  });
+//     responsive: [
+//       {
+//         breakpoint: 992,
+//         settings: {
+//           arrows: false,
+//           dots: true,
+//         },
+//       },
+//     ],
+//   });
+// });
+
+const slider = tns({
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  controls: false,
+  nav: false,
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev');
+});
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo('next');
 });
